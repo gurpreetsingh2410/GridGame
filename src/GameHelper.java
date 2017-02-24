64ed98a6af27fadf0a1604b62e49795fc8c4fd42
gurpreetsingh2410 <gurpreetsingh2410@gmail.com>
2017-02-24 03:52:28 -0500

    Improving Low Cohesion and High Coupling

--- src/GameHelper.java
+++ src/GameHelper.java
@@ -4,9 +4,11 @@ import java.util.Scanner;
  * Created by root on 24/02/17.
  */
 public class GameHelper {
+    int guess;
     Scanner sc = new Scanner(System.in);
 
-    public void UserInput(String askInput){
-
+    public int UserInput(){
+        guess = sc.nextInt();
+        return guess;
     }
 }
--- src/dotCom.java
+++ src/dotCom.java
@@ -1,4 +1,3 @@
-import java.util.Scanner;
 /**
  * Created by root on 18/02/17.
  */
@@ -6,13 +5,16 @@ public class dotCom {
    static String result;
    static int x = 0;
     public static void main (String args[]){
-        Scanner sc = new Scanner(System.in);
         dotComGame dgame = new dotComGame();
+        GameHelper gh = new GameHelper();
+
         dgame.setLocation();
-        do{
-        System.out.printf("Please enter any number to play this game\n");
-        result = dgame.checkGuess(sc.nextInt());
-        }while(!"kill".equals(result));
+
+       do{
+       System.out.println("Enter any number");
+        int guess = gh.UserInput();
+        result = dgame.checkGuess(guess);
+       }while(!"kill".equals(result));
 
     }
 }
